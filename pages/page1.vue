9927266c7e45df872bb42c7d509bcf546dbc9c2b
Andy <andy@Andys-MacBook-Pro.local>
2024-08-05 12:36:08 -0400

    init

---
+++ pages/page1.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const click = async () => {
+  window.location.href = '/page2'
+}
+</script>
+
+<template>
+  <div>
+    <h1>Page 1</h1>
+    <button @click="click">Go to Page 2</button>
+  </div>
+</template>
---
+++ pages/page2.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const click = async () => {
+  window.location.href = '/page1'
+}
+</script>
+
+<template>
+  <div>
+    <h1>Page 2</h1>
+    <button @click="click">Go to Page 1</button>
+  </div>
+</template>
